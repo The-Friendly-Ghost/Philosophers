3a88d8e09f6a5be65c42a923befd7db148b3c5ff
Casper <casperaronpost@protonmail.com>
2022-08-08 20:46:17 +0200

    Added init_mutex function

--- src/initiate_data_struct.c
+++ src/initiate_data_struct.c
@@ -6,7 +6,7 @@
 /*   By: cpost <cpost@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/12 12:20:19 by cpost         #+#    #+#                 */
-/*   Updated: 2022/08/07 17:27:35 by casper        ########   odam.nl         */
+/*   Updated: 2022/08/08 20:27:50 by casper        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,45 @@
 #include <limits.h>
 
 /**
- * Initiate mutexes in the data struct
+ * Initiates array of mutexes (each mutex is a fork). For every philosopher,
+ * one fork mutex is initiated.
  * @param data Struct with the program data in it
  * @return True if initiation without error. False if there
  * is an error somewhere.
  */
+static bool	initiate_forks(unsigned int amount_philos, mutex *fork_array)
+{
+	unsigned int	fork_id;
+	
+	fork_array = calloc(amount_philos, sizeof(mutex)); //IS CALLOC ALLOWED??? CHECK THIS!
+	if (fork_array == NULL)
+		return (false);
+	fork_id = 0;
+	while (fork_id < amount_philos)
+	{
+		pthread_mutex_init(&fork_array[fork_id], NULL);
+		fork_id++;
+	}
+	return (true);
+}
+
+/**
+ * Initiate mutexes in the data struct
+ * @param data Struct with the program data in it
+ * @return True if initiation without error. False if there
+ * is an error somewhere.
+ *  @note Functions location :
+ * 'initiate_forks' > same file
+ */
 static bool	initiation_of_mutexes_in_data_struct(t_data *data)
 {
-	if (initiate_array_of_forks(data->amount_philosophers, &data->forks) == false)
+	if (initiate_forks(data->amount_philosophers, data->forks) == false)
 		return (false);
-	
+	if (pthread_mutex_init(&data->philo_dead_lock, NULL) != 0)
+		return (false);
+	if (pthread_mutex_init(&data->write_lock, NULL) != 0)
+		return (false);
+	return (true);
 }
 
 /**
@@ -63,12 +92,15 @@ static bool	convert_str_to_int(const char *argument, unsigned int *data)
 }
 
 /**
- * Initialize arguments into the data struct.
+ * Initialize arguments into the data struct. 
  * @param data Struct with the program data in it
  * @param argument_count Amount of arguments
  * @param argument Arguments inputted on the command line
  * @return t_data struct if arguments are valid. NULL if arguments are 
  * not valid. 
+ * @note Functions location :
+ * 'an_argument_is_zero' > utils.c
+ * 'convert_str_to_int' > same file
  */
 static bool	parse_arguments(t_data *data, int argument_count, char **argument)
 {
@@ -99,6 +131,9 @@ static bool	parse_arguments(t_data *data, int argument_count, char **argument)
  * @param argument Arguments inputted on the command line
  * @return t_data struct if arguments are valid. NULL if arguments are 
  * not valid. 
+ * @note Functions location :
+ * 'parse_arguments' > same file
+ * 'initiation_of_mutexes_in_data_struct' > same file
  */
 t_data	*initiate_data_struct(int argument_count, char **argument)
 {
