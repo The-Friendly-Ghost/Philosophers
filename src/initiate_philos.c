19db8484c1d2fd850ba5f68abf8d3e004b93f6eb
Casper <casperaronpost@protonmail.com>
2022-08-08 20:47:12 +0200

    Added init_philo function (not working yet)

--- src/initiate_philos.c
+++ src/initiate_philos.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   initiate_mutexes.c                                 :+:    :+:            */
+/*   initiate_philos.c                                  :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: casper <casper@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/07 17:19:35 by casper        #+#    #+#                 */
-/*   Updated: 2022/08/07 17:19:36 by casper        ########   odam.nl         */
+/*   Updated: 2022/08/08 20:42:13 by casper        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@
  * @param data Struct with the program data in it
  * @return nothing 
  */
-void	*initiate_data_struct(int argument_count, char **argument)
+t_philo	**initiate_philo_struct(t_data *data)
 {
-	t_data	*data;
+	t_philo			**philo;
+	unsigned int	i;
 
-	if (argument_count != 5 && argument_count != 6)
+	philo = calloc(data->amount_philosophers, sizeof(t_philo));
+	if (philo == NULL)
+		return (NULL);
+	i = 0;
+	while (i < data->amount_philosophers)
 	{
-		printf("Invalid amount of arguments\n");
-		return (NULL);
+		philo[i]->id = i + 1;
+		philo[i]->x_eaten = 0;
+		philo[i]->data = data;
+		philo[i]->left_fork = &data->forks[i];
+		philo[i]->right_fork = &data->forks[(i + 1) % data->amount_philosophers];
+		philo[i]->last_meal = //time function here
+		i++;
 	}
-	data = malloc(sizeof(t_data));
-	if (data == NULL)
-		return (NULL);
-	if (parse_arguments(data, argument_count, argument) == false)
-	{
-		free(data);
-		return (NULL);
-	}
-	return (data);
+	return (philo);
 }
