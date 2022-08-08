7efffb0b5d330d2a6a87c92c12fb0c90a3641a05
Casper <casperaronpost@protonmail.com>
2022-08-08 20:47:49 +0200

    added a few lines.. Not working yet

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cpost <cpost@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/11 13:47:53 by cpost         #+#    #+#                 */
-/*   Updated: 2022/07/29 14:57:24 by cpost         ########   odam.nl         */
+/*   Updated: 2022/08/08 20:27:59 by casper        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,28 @@ void	leaks_func(void)
 	system("leaks philo");
 }
 
+/**
+ * Main
+ * @param argument_count Amount of arguments
+ * @param arguments Arguments inputted on the command line
+ * @return SUCCESS if program succesfully ends. FAILURE if there's an
+ * error somewhere. 
+ * @note Functions location :
+ * 'initiate_data_struct' > initiate_data_struct.c
+ */
 int	main(int argument_count, char **arguments)
 {
 	t_data	*data;
-	//t_philo	*philos;
+	t_philo	**philos;
 
 	//atexit(leaks_func);
-	//philos = NULL;
+	philos = NULL;
 	data = initiate_data_struct(argument_count, arguments);
 	if (data == NULL)
 		return (FAILURE);
-	//philos = init_philos(data);
-	//if (philos = NULL)
-	//	return (FAILURE);
+	philos = initiate_philo_struct(data);
+	if (philos = NULL)
+		return (FAILURE);
 	free(data);
 	return (SUCCES);
 }
